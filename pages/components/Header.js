3b473f30e2d70d6220ec08e974479834b3510581
stephen omarrah <stephenomarrah@gmail.com>
2022-12-20 21:00:29 -0600

    fixed for deploy

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -7,8 +7,8 @@ export default function Header({handleClick}) {
     <div style={{position:"sticky",top:"0", display:"flex", flexDirection:"row", flexWrap: "nowrap",alignContent: "center",justifyContent: "space-between",alignItems: "center", backdropFilter:"blur(3px)", zIndex:"1", height:"100px"}}>
         <div onClick={()=>handleClick(3)} style={{height:"100px", width:"100px", userSelect:"none", WebkitUserDrag:"none", zIndex:"0", position:"relative"}}>
             
-            <Image  className="animated"src="/images/logo.gif" alt="energy spiral gif" fill objectFit="cover" objectPosition="left"/>
-            <Image className="static" src="/images/logo.png" alt="energy spiral gif" fill objectFit="cover" objectPosition="left"/>
+            <Image  className="animated" width={'100'} height={'100'} src="/images/logo.gif" alt="energy spiral gif" />
+            <Image className="static" src="/images/logo.png" alt="energy spiral gif" width={'100'} height={'100'}/>
         </div>
         <div style={{display:"flex", justifyContent: "space-around"}}>
             <h3 onClick={()=>handleClick(1)} className="links" style={{margin:"10px auto", padding:"10px"}}>About</h3>
