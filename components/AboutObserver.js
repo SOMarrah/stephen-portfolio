4640764418bc1dc84f7133152533cb2ae3d619de
stephen omarrah <stephenomarrah@gmail.com>
2022-12-20 21:23:00 -0600

    added conditional rendering

--- components/AboutObserver.js
+++ components/AboutObserver.js
@@ -13,15 +13,15 @@ export default function AboutObserver({data}) {
   return (
     <>
     {data ?
-    <div ref={myRef} style={{margin:"20px", padding:"8px", display: "flex",flexFlow: "row nowrap", placeContent: "center", alignItems: "center", opacity: cardVis ? 1 : 0, transition:"all ease-in-out 2s"}} >
-    <div>
-        <img src="/images/d4.png" alt="d4 dice" style={{height:"auto", width:"100px"}}/>
-    </div>
-    <div style={{margin:"10px", padding:"15px", borderRadius:"10px", zIndex:"0"}}>
-        <h4 style={{textAlign:"center", marginBottom:"20px", color:"#F45B7E"}}>{data.title}</h4>
-        <p style={{background:"grey", margin:"10px", padding:"15px", borderRadius:"10px", zIndex:"0"}}>{data.text}</p>
-    </div>
-    </div>
+        <div ref={myRef} style={{margin:"20px", padding:"8px", display: "flex",flexFlow: "row nowrap", placeContent: "center", alignItems: "center", opacity: cardVis ? 1 : 0, transition:"all ease-in-out 2s"}} >
+        <div>
+            <img src="/images/d4.png" alt="d4 dice" style={{height:"auto", width:"100px"}}/>
+        </div>
+        <div style={{margin:"10px", padding:"15px", borderRadius:"10px", zIndex:"0"}}>
+            <h4 style={{textAlign:"center", marginBottom:"20px", color:"#F45B7E"}}>{data.title}</h4>
+            <p style={{background:"grey", margin:"10px", padding:"15px", borderRadius:"10px", zIndex:"0"}}>{data.text}</p>
+        </div>
+        </div>
     : null}
     </>
   )
