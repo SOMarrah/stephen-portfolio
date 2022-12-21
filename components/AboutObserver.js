9c8e0727bc2f9103094f990933495666dd5af4fa
stephen omarrah <stephenomarrah@gmail.com>
2022-12-20 21:36:22 -0600

    test

--- components/AboutObserver.js
+++ components/AboutObserver.js
@@ -3,8 +3,8 @@ import { useRef, useEffect, useState } from 'react';
 export default function AboutObserver({data}) {
     if(!data){
         data = {
-            "title": "UAV Online",
-            "text": "I am a USMC veteran, deployed to Afghanistan, and I flew drones for 4 years. This was my first experience working intensively with technology along with collaborating in cross-team environments, and holding leadership positions. "
+            title: "UAV Online",
+            text: "I am a USMC veteran, deployed to Afghanistan, and I flew drones for 4 years. This was my first experience working intensively with technology along with collaborating in cross-team environments, and holding leadership positions. "
         }
     }
     const myRef = useRef(null);
@@ -24,7 +24,7 @@ export default function AboutObserver({data}) {
             <img src="/images/d4.png" alt="d4 dice" style={{height:"auto", width:"100px"}}/>
         </div>
         <div style={{margin:"10px", padding:"15px", borderRadius:"10px", zIndex:"0"}}>
-            <h4 style={{textAlign:"center", marginBottom:"20px", color:"#F45B7E"}}>{data.title}</h4>
+            <h4 style={{textAlign:"center", marginBottom:"20px", color:"#F45B7E"}}> {data.title} </h4>
             <p style={{background:"grey", margin:"10px", padding:"15px", borderRadius:"10px", zIndex:"0"}}>{data.text}</p>
         </div>
         </div>
